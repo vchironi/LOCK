f6f0246d83a87cd8009ad4231331a3d794adc3cf
vchironi <vchironi@gmail.com>
2025-03-28 16:01:48 +0100

    commit_2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "ec2_example" {
-    ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+    ami           = "ami-00101c3551c99b710" # Amazon Linux 2 AMI
     instance_type = "t2.micro"
     tags = {
         Name = "MyEC2Instance_1_"
