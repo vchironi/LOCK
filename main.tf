651256197ba347c384431bcad99c1a8a54245e1e
vchironi <vchironi@gmail.com>
2025-03-28 16:10:08 +0100

    commit_4

--- main.tf
+++ main.tf
@@ -1,10 +1,26 @@
+resource "aws_vpc" "private_vpc" {
+  cidr_block = "10.0.0.0/16"
 
+  tags = {
+    Name = "my-private-vpc"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.private_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "my-private-subnet"
+  }
+}
 resource "aws_instance" "ec2_example" {
-    ami           = "ami-00101c3551c99b710" # Amazon Linux 2 AMI
-    instance_type = "t2.micro"
-    subnet_id = "subnet-04654876f3e187c4a" 
-    tags = {
-        Name = "MyEC2Instance_1_"
-    }
-  
+  ami           = "ami-00101c3551c99b710"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnet.id
+
+  tags = {
+    Name = "MyEC2Instance_1_"
+  }
 }
